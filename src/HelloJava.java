98292b1cacb78085e9e00db6fc6566a9412fbcb6
pra152 <pravatdas211@gmail.com>
2021-07-29 20:51:36 +0530

    Addition

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -4,6 +4,10 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java!!");
 		System.out.println("Test in git!!");
+		int a=1;
+		int b=2;
+		int c=a+b;
+		System.out.println(c);
 	}
 
 }
