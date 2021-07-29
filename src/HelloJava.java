059e12b22cb89eedd1235f6491549bbf31b28b42
pra152 <pravatdas211@gmail.com>
2021-07-29 21:09:19 +0530

    Add: Added .gitignore file & substration of two no.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -4,6 +4,11 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java!!");
 		System.out.println("Test in git!!");
+		int a=2;
+		int b=3;
+		int c=a-b;
+		System.out.println(c);
+		
 	}
 
 }
